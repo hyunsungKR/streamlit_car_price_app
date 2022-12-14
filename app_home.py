4708effc45579f40ae24fb81caf72a3fd937a420
hyunsungKR <a01090693658@gmail.com>
2022-12-14 12:07:31 +0900

    홈화면 수정

--- app_home.py
+++ app_home.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 
 def run_home_app() :
+    st.text('환영합니다.')
     st.text(' 이 앱은 고객데이터와 자동차 구매 데이터에 대한 내용입니다.\n데이터 분석 및 고객 정보를 넣으면, 얼마 정도의 차를 구매할지를 예측해줍니다.')
     img_url = 'https://www.motorgraph.com/news/photo/201905/22564_72789_5839.jpg'
     st.image(img_url)
